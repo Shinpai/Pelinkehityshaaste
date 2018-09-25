d3c625973a6b5e90daa427c1efad282eef749a6e
Shinpai <juutilainen.harri@gmail.com>
2018-09-25 13:07:27 +0300

    testibuildit

---
+++ Robomop/Assets/CameraControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {		
+		this.transform.position = new Vector3(3,-2,-7);
+		this.transform.rotation = Quaternion.Euler(new Vector3(10, -10, 0));			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
